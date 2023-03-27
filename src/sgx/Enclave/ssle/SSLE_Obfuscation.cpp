a2bc25686fb87106cbb33aa9f37edfb38e0febb6
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-03-27 16:32:52 +0200

    modified header file.

--- src/sgx/Enclave/ssle/SSLE_Obfuscation.cpp
+++ src/sgx/Enclave/ssle/SSLE_Obfuscation.cpp
@@ -8,29 +8,7 @@
 
 using namespace std;
 
-class SSLE_Obfuscation : public SSLE
-{
-    private:
-//    vector<Node> nodes; // a list of participants.
-    vector<string> comm_values; // committed values correspoding to nodes
-    uint64_t secret = 0x93064E905C127FE5; // key
-    uint64_t hash = 0xA4BDE5C4A05E6256; // hash (constant)
-    uint64_t lcg = 0; // needed for pprf.
-    string randKey;
-    string punctKey;
-    int num;
-    uint32_t leader_id;
 
-
-    public:
-        void initialize(const int *);
-        void electLeader();
-        static int verifyLeader();
-        string getRandKey();
-        string getPunctKey();
-        string getCommValue(int);
-
-};
 
 void SSLE_Obfuscation::initialize(const int *participants)
 {
