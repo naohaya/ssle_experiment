7a3dd5cacb335a458bf09e18b765de9f5ed51dfe
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-04-25 14:33:35 +0200

    a small update.

--- src/sgx/App/App.cpp
+++ src/sgx/App/App.cpp
@@ -218,7 +218,7 @@ int main(int argc, char *argv[])
         if (public_key == NULL) {
             std::cout << "public key is NULL" << std::endl;
         } else {
-            std::cout << (char *)public_key << std::endl;
+            std::cout << "public key: " << (char *)public_key << std::endl;
         }
 
 
