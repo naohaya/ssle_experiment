1a94b388372cc9082a8ba556774ad50d35f384ca
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-05-05 15:49:23 +0200

    debug.

--- src/sgx/Enclave/Enclave.cpp
+++ src/sgx/Enclave/Enclave.cpp
@@ -53,11 +53,10 @@ int ecall_election(const uint64_t *prfkey,
 
 	ssleobf.initialize(prfkey, num_nodes);
 
-	ocall_print("start election");
 	ssleobf.electLeader();
-	ocall_print("end election");
 
 	commitValue = ssleobf.getCommValue(0);
+	ocall_print(commitValue);
 	memcpy(output, &commitValue, sizeof(uint64_t));
 
 	result = ssleobf.getPunctKey();
