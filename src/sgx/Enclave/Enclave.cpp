280c0eeaf6ea711aa038fb4fc9afcce7cc170b7a
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-06-06 17:57:59 +0200

    debug.

--- src/sgx/Enclave/Enclave.cpp
+++ src/sgx/Enclave/Enclave.cpp
@@ -84,8 +84,8 @@ int ecall_election(const uint64_t *prfkey,
 	// key = create_aes_key();
 
 	// int encret = encrypt_aes(key, inData, sizeof(uint8_t), outData);
-	ocall_print("Encrypted data:");
-	ocall_print((char *)outData);
+	// ocall_print("Encrypted data:");
+	// ocall_print((char *)outData);
 //	ocall_print((char *)outlen);
 	
 //	ocall_print(ssleobf.getRandKey().c_str()); // random key
