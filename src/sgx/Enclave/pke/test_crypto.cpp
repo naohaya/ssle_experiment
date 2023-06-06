9d34d2e2b121d707600f4ea8667efdbf12a18610
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-06-06 16:56:22 +0200

    debug.

--- src/sgx/Enclave/pke/test_crypto.cpp
+++ src/sgx/Enclave/pke/test_crypto.cpp
@@ -75,7 +75,7 @@ int test_encrypt(void *pkey, unsigned char *outData)
     char *pin_data = "Hello World!";
     size_t out_len = 0;
 
-    ocall_print(strlen(pin_data));
+    ocall_print(std::to_string(strlen(pin_data)).c_str());
 
     sgx_status_t ret_get_output_len = sgx_rsa_pub_encrypt_sha256(pkey, NULL, &out_len, (unsigned char *)pin_data, strlen(pin_data));
 
