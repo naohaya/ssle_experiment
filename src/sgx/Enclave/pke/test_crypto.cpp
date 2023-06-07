7d62c6a9830fe27a25bf0beecbc49c78998c7e2e
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-06-07 11:05:10 +0200

    debug.

--- src/sgx/Enclave/pke/test_crypto.cpp
+++ src/sgx/Enclave/pke/test_crypto.cpp
@@ -186,8 +186,8 @@ void test_crypto()
         // ocall_print((char *)p_q);
     }
 
-    //void *private_key = NULL;
-    unsigned char private_key[256];
+    void *private_key = NULL;
+    //unsigned char private_key[256];
 
     sgx_status_t ret_create_private_key = sgx_create_rsa_priv2_key(n_byte_size, sizeof(e), (unsigned char *)&e, p_p, p_q, p_dmp1, p_dmq1, p_iqmp, &private_key);
 
@@ -197,8 +197,8 @@ void test_crypto()
         ocall_print(std::to_string(ret_create_private_key).c_str());
     }
 
-    //void *public_key = NULL;
-    unsigned char public_key[256];
+    void *public_key = NULL;
+    //unsigned char public_key[256];
 
     sgx_status_t ret_create_public_key = sgx_create_rsa_pub1_key(n_byte_size, sizeof(e), p_n, (unsigned char *)&e, &public_key);
 
