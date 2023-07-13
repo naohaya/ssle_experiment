6f38fb380788bd718453de8b599c1719f3de3bcb
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-07-13 11:41:14 +0200

    debug.

--- src/sgx/Enclave/crypto/crypto.cpp
+++ src/sgx/Enclave/crypto/crypto.cpp
@@ -34,7 +34,7 @@ int encrypt_aes(void *dataIn, size_t len, char *dataOut, size_t lenOut)
     to produce the actual unique counter block for encryption */
 
     sgx_read_rand(p_dst + SGX_AESGCM_MAC_SIZE, SGX_AESGCM_IV_SIZE);
-
+    ocall_print("hoge");
     sgx_rijndael128GCM_encrypt(
         &gcmkey,
         clairText, len,
