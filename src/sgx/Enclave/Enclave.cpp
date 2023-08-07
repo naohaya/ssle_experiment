c02cbb557f10c35b9097a30008bafabaa56e1cb5
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2023-08-07 15:48:03 +0200

    debug.

--- src/sgx/Enclave/Enclave.cpp
+++ src/sgx/Enclave/Enclave.cpp
@@ -63,7 +63,7 @@ int ecall_election(const uint64_t *prfkey,
 	ssleobf.electLeader(*prfkey);
 
 	commitValue = ssleobf.getCommValue(0);
-	ocall_print((char *)&commitValue);
+	ocall_print((unsigned char)commitValue);
 	memcpy(output, &commitValue, sizeof(uint64_t)); // copy the committed value to the outside pointer.
 
 	result = ssleobf.getPunctKey();
